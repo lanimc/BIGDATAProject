2800b2349bb64935cfd43f64f2c6eb332f83c193
lanimc <lanimc@me.com>
2016-05-16 04:22:00 -0400

    updating summary stats

--- summarystats.py
+++ summarystats.py
@@ -14,13 +14,19 @@ if __name__ == "__main__":
     sc = SparkContext(appName="SummaryStatisticsExample")  # SparkContext
 
   
-    mat = sc.parallelize(JoinedDataset)  # an RDD of Vectors
+    bike = sc.textFile("s3://irm238FinalProject/input/*-citibike-tripdata.csv")  # an RDD of Vectors
 
+	taxi = sc.textFile("s3://irm238FinalProject/input/*-citibike-tripdata.csv") 
+   
     # Compute column summary statistics.
-    summary = Statistics.colStats(mat)
+    summary = Statistics.colStats(bike)
     print(summary.mean())  # a dense vector containing the mean value for each column
     print(summary.variance())  # column-wise variance
     print(summary.numNonzeros())  # number of nonzeros in each column
     
+    summary = Statistics.colStats(taxi)
+    print(summary.mean())  # a dense vector containing the mean value for each column
+    print(summary.variance())  # column-wise variance
+    print(summary.numNonzeros())  # number of nonzeros in each column
 
     sc.stop()
