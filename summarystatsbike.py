ba7e9b62e6409954162cc04aedf76c27fd0b0846
lanimc <lanimc@me.com>
2016-05-16 04:33:59 -0400

    new summary

---
+++ summarystatsbike.py
@@ -0,0 +1,26 @@
+#Summary stats of data
+# source:https://github.com/apache/spark/blob/master/examples/src/main/python/mllib/summary_statistics_example.py
+
+from __future__ import print_function
+
+from pyspark import SparkContext
+
+import numpy as np
+
+from pyspark.mllib.stat import Statistics
+
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="SummaryStatisticsExample")  # SparkContext
+
+  
+    bike = sc.textFile("s3://irm238finalproject/input/*-citibike-tripdata.csv")  # an RDD of Vectors
+   
+    # Compute column summary statistics.
+    summary = Statistics.colStats(bike)
+    print(summary.mean())  # a dense vector containing the mean value for each column
+    print(summary.variance())  # column-wise variance
+    print(summary.numNonzeros())  # number of nonzeros in each column
+    
+
+    sc.stop()
